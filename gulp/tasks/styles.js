7f76333e24bdb91884b7d120b40b930da409dee7
John Doe <john.doe@gmail.com>
2018-01-23 10:07:15 -0500

    PostCSS + PostCSS-Import

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	postcss = require('gulp-postcss'),
+	cssimport = require('postcss-import');
 
 gulp.task('styles', function() {
 
 	return gulp.src('./app/assets/css/build/styles.css')
+		.pipe(postcss([cssimport]))
 		.pipe(gulp.dest('./app/assets/css'));
 });
