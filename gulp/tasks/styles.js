fba7197206420a3b4380be99efc4d633de53f4eb
John Doe <john.doe@gmail.com>
2018-01-23 10:15:15 -0500

    Autoprefixer

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
-	cssimport = require('postcss-import');
+	cssimport = require('postcss-import'),
+	cssvars = require('postcss-simple-vars'), _
+	autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
 
 	return gulp.src('./app/assets/css/build/styles.css')
-		.pipe(postcss([cssimport]))
+		.pipe(postcss([cssimport, cssvars, autoprefixer]))
 		.pipe(gulp.dest('./app/assets/css'));
 });
