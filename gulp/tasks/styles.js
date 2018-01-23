16c1c254e01758fb6db0c21ab97948a070a797e9
John Doe <john.doe@gmail.com>
2018-01-23 10:18:57 -0500

    PostCSS-Nested

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
 	cssimport = require('postcss-import'),
-	cssvars = require('postcss-simple-vars'), _
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
 	autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
 
 	return gulp.src('./app/assets/css/build/styles.css')
-		.pipe(postcss([cssimport, cssvars, autoprefixer]))
+		.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/assets/css'));
 });
