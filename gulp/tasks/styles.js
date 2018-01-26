2fa92192bc015552fb6f5c5d96ca7b2ad967ab18
John Doe <john.doe@gmail.com>
2018-01-25 19:27:55 -0500

    Completed Large Hero and Intro Sections

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,16 @@ var gulp = require('gulp'),
 	cssimport = require('postcss-import'),
 	cssvars = require('postcss-simple-vars'),
 	nested = require('postcss-nested'),
-	autoprefixer = require('autoprefixer');
+	autoprefixer = require('autoprefixer'),
+	mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
 
 	return gulp.src('./app/assets/css/build/styles.css')
-		.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./app/assets/css'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,17 +1,34 @@
 var gulp = require('gulp'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
 
 
 gulp.task('watch', function() {
 
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
 	watch('./app/index.html', function() {
-		console.log("./app/index.html changed event. BrowserSync here.");
+		//console.log("./app/index.html changed event. BrowserSync here.");
+		browserSync.reload();
+
 	});
 
 	watch('./app/assets/css/build/**/*.css', function() {
 		//console.log("./app/assets/css/build/**/*.css changed event. PostCSS or SASS here.");
 
-		gulp.start('styles'); // Run styles task found in 'gulp/tasks/styles.js
+		gulp.start('cssInject'); // Run cssInject task
 	});
 
+});
+
+// Run dependency tasks (i.e param 2; ex. 'styles' task found in 'gulp/tasks/styles.js') before 
+// running the specified task (i.e. 'cssInject')
+gulp.task('cssInject', ['styles'], function() {
+	return gulp.src('./app/assets/css/styles.css')
+		.pipe(browserSync.stream());
 });
