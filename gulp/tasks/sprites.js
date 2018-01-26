f8206fc583557e61833e7dffb0a0254587b26997
Raymond Repito <raymond.repito@gmail.com>
2018-01-26 15:03:18 -0500

    successful sprites part 2

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -19,10 +19,10 @@ gulp.task('createSprite', function() {
 	return gulp.src('./app/assets/img/icons/**/*.svg')
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/assets/img/sprite/'));
+
 });
 
 gulp.task('copySpriteCSS', function() {
-
 	return gulp.src('./app/assets/img/sprite/css/*.css')
 		.pipe(rename(function(path) {
 			path.basename = "_" + path.basename;
