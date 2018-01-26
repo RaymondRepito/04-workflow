cba90b63dcab8aee9bad33513515351155c969ac
Raymond Repito <raymond.repito@gmail.com>
2018-01-26 16:27:43 -0500

    Completed Sprites Automation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 	svgSprite = require('gulp-svg-sprite'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	del = require('del');
 
 var config = {
 	mode: {
 		css : {
+			sprite: 'svg/sprite.svg',
 			render : {
 				css: {
 					template: './gulp/templates/sprite.css'
@@ -14,18 +16,26 @@ var config = {
 	}
 }
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+	return del(['./app/assets/img/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
 
 	return gulp.src('./app/assets/img/icons/**/*.svg')
 		.pipe(svgSprite(config))
-		.pipe(gulp.dest('./app/assets/img/sprite/'));
+		.pipe(gulp.dest('./app/assets/img/sprites/'));
 
 });
 
-gulp.task('copySpriteCSS', function() {
-	return gulp.src('./app/assets/img/sprite/css/*.css')
+/* let createSprite finish first before running copySpriteCSS */
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+	return gulp.src('./app/assets/img/sprites/css/*.css')
 		.pipe(rename(function(path) {
 			path.basename = "_" + path.basename;
 		}))
 		.pipe(gulp.dest('./app/assets/css/build/sprites'));
 });
+
+/* run both createSprite and copySpriteCSS */
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteCSS']);
